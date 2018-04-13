cd75fd130a9eeef08f475c6844682724e4daae43
sana054 <36442395+sana054@users.noreply.github.com>
2018-04-13 09:46:14 +0100

    add request service
    add request service to display json data via a rest Api
    add import in app module to see the new service file 
    edit in home component to get the result 
    display data using home.component.html

---
+++ home.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import {RequestsService} from "../requests.service";
+import {map} from 'rxjs/operator/map';
+import {Observable} from 'rxjs/Observable';
+import { CommonModule } from "@angular/common";
+
+/**
+ * This class represents the lazy loaded HomeComponent.
+ */
+@Component({
+  moduleId: module.id,
+  selector: 'sd-home',
+  templateUrl: 'home.component.html',
+  styleUrls: ['home.component.css'],
+})
+export class HomeComponent implements OnInit {
+
+    ids:any;
+  response:any;
+
+
+
+  constructor(public rq: RequestsService) {}
+
+
+  ngOnInit() {
+    console.log("xxxxxx")
+    this.rq.requests().subscribe(res=>{
+      this.ids = res.map(res=>res);
+      console.log(res);
+      this.response = res.map(res=>res.responseRequest);
+      console.log(this.response);
+
+    })
+
+
+
+  }
+
+
+
+}
